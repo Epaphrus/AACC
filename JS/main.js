06af637d2b6aa566c802313ce988f702233e76c7
Maxwel Khadambi <maxwelkhadambi@gmail.com>
2024-08-19 17:15:14 +0300

    updated changes for the home file

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,22 @@
 // js/main.js
 // Add your custom JavaScript here
-document.addEventListener('DOMContentLoaded', function() {
-    // Your code here
+document.addEventListener('DOMContentLoaded', function () {
+    // Your code here    
+
+
+    // BACK TO TOP ARROW 
+    var backToTop = document.getElementById('back-to-top');
+
+    window.addEventListener('scroll', function () {
+        if (window.pageYOffset > 100) {
+            backToTop.style.display = 'block';
+        } else {
+            backToTop.style.display = 'none';
+        }
+    });
+
+    backToTop.addEventListener('click', function (e) {
+        e.preventDefault();
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
 });
