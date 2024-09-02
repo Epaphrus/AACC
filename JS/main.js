cf459f0deb83fdf732ca23404f8a876ed2799ac4
Maxwel Khadambi <maxwelkhadambi@gmail.com>
2024-09-02 16:30:07 +0300

    hero section globe

--- JS/main.js
+++ JS/main.js
@@ -3,7 +3,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Your code here    
 
-
     // BACK TO TOP ARROW 
     var backToTop = document.getElementById('back-to-top');
 
@@ -39,39 +38,6 @@ document.addEventListener('DOMContentLoaded', function () {
         once: true
     });
 
-
-    // UPCOMING EVENTS COUNT DOWN index.HTML
-    // const countdownElements = document.querySelectorAll('.countdown');
-
-    // countdownElements.forEach(element => {
-    //     const eventDate = new Date(element.dataset.date).getTime();
-
-    //     const updateCountdown = () => {
-    //         const now = new Date().getTime();
-    //         const distance = eventDate - now;
-
-    //         const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    //         const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    //         const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    //         const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    //         element.innerHTML = `
-    //             <div class="countdown-item">${days}d</div>
-    //             <div class="countdown-item">${hours}h</div>
-    //             <div class="countdown-item">${minutes}m</div>
-    //             <div class="countdown-item">${seconds}s</div>
-    //         `;
-
-    //         if (distance < 0) {
-    //             clearInterval(interval);
-    //             element.innerHTML = "Event has started!";
-    //         }
-    //     };
-
-    //     updateCountdown();
-    //     const interval = setInterval(updateCountdown, 1000);
-    // });
-
     function updateCountdown() {
         const countdownElements = document.querySelectorAll('.countdown');
 
@@ -100,5 +66,84 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Call the function immediately and then set interval
     updateCountdown();
-    setInterval(updateCountdown, 1000);    
+    setInterval(updateCountdown, 1000);
+
+    // HERO SECTION GLOBE 
+    am5.ready(function () {
+
+        // Create root element
+        // https://www.amcharts.com/docs/v5/getting-started/#Root_element
+        var root = am5.Root.new("chartdiv");
+
+
+        // Set themes
+        // https://www.amcharts.com/docs/v5/concepts/themes/
+        root.setThemes([
+            am5themes_Animated.new(root)
+        ]);
+
+
+        // Create the map chart
+        // https://www.amcharts.com/docs/v5/charts/map-chart/
+        var chart = root.container.children.push(am5map.MapChart.new(root, {
+            panX: "rotateX",
+            panY: "rotateY",
+            projection: am5map.geoOrthographic(),
+            paddingBottom: 20,
+            paddingTop: 20,
+            paddingLeft: 20,
+            paddingRight: 20
+        }));
+
+
+        // Create main polygon series for countries
+        // https://www.amcharts.com/docs/v5/charts/map-chart/map-polygon-series/
+        var polygonSeries = chart.series.push(am5map.MapPolygonSeries.new(root, {
+            geoJSON: am5geodata_worldLow
+        }));
+
+        polygonSeries.mapPolygons.template.setAll({
+            tooltipText: "{name}",
+            toggleKey: "active",
+            interactive: true
+        });
+
+        polygonSeries.mapPolygons.template.states.create("hover", {
+            fill: root.interfaceColors.get("primaryButtonHover")
+        });
+
+
+        // Create series for background fill
+        // https://www.amcharts.com/docs/v5/charts/map-chart/map-polygon-series/#Background_polygon
+        var backgroundSeries = chart.series.push(am5map.MapPolygonSeries.new(root, {}));
+        backgroundSeries.mapPolygons.template.setAll({
+            fill: root.interfaceColors.get("alternativeBackground"),
+            fillOpacity: 0.1,
+            strokeOpacity: 0
+        });
+        backgroundSeries.data.push({
+            geometry: am5map.getGeoRectangle(90, 180, -90, -180)
+        });
+
+
+        // Create graticule series
+        // https://www.amcharts.com/docs/v5/charts/map-chart/graticule-series/
+        var graticuleSeries = chart.series.push(am5map.GraticuleSeries.new(root, {}));
+        graticuleSeries.mapLines.template.setAll({ strokeOpacity: 0.1, stroke: root.interfaceColors.get("alternativeBackground") })
+
+
+        // Rotate animation
+        chart.animate({
+            key: "rotationX",
+            from: 0,
+            to: 360,
+            duration: 30000,
+            loops: Infinity
+        });
+
+
+        // Make stuff animate on load
+        chart.appear(1000, 100);
+
+    }); // end am5.ready()
 });
